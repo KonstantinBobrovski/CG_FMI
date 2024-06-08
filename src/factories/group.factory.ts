3097cb611e2058df486ca934b8a67e95dc9328b9
Velin Kurtev <velin.kurtev@dreambox.com>
2024-06-09 02:23:11 +0300

    Removed color property, added copy group

--- src/factories/group.factory.ts
+++ src/factories/group.factory.ts
@@ -17,10 +17,9 @@ export class GroupFactory {
   private static getBaseProperties(groupName: string): Record<string, Property> {
     return [
       new NameProperty("groupName", groupName),
-      new ColorProperty("fill", "#000000", "Group Color"),
       new PercentageProperty("opacity", 1, "Group Opacity"),
       new NumberProperty("z-index", 1, "z-index"),
-    ].reduce((prev, curr) => ({ ...prev, [curr.name]: { ...curr } }), {});;
+    ].reduce((prev, curr) => ({ ...prev, [curr.name]: curr }), {});;
   }
 }
 
--- src/figures-container.ts
+++ src/figures-container.ts
@@ -1,7 +1,10 @@
+import { dragAndDropBootstrap } from ".";
 import { BaseFigureFactory } from "./factories/base-figure.factory";
 import { GroupFactory } from "./factories/group.factory";
 import Figure from "./models/figure";
 import Group from "./models/group";
+import { NameProperty, Property } from "./models/properties";
+import { Copy } from "./ui/actions";
 import { closePropPane, createGroupPropPane, createPropPane } from "./ui/create-prop-pane";
 const svgRoot = document.querySelector<HTMLElement>("#svg-root")!;
 const treemap = document.querySelector<HTMLElement>("#treemap-figures")!;
@@ -11,10 +14,11 @@ export const figuresContainer = {
   figures: [] as Figure[],
   groups: [] as Group[],
   refreshOrder() {
-      this.figures.sort((fig1, fig2) => +fig1.properties["z-index"].value - +fig2.properties["z-index"].value);
-      this.clearElement(svgRoot);
-      this.clearElement(treemap);
-      this.refreshTree();
+    this.figures.sort((fig1, fig2) => +fig1.properties["z-index"].value - +fig2.properties["z-index"].value);
+    this.groups.sort((group1, group2) => +group1.properties["z-index"].value - +group2.properties["z-index"].value);
+    this.clearElement(svgRoot);
+    this.clearElement(treemap);
+    this.refreshTree();
   },
   refreshTree() {
     this.clearElement(treemap);
@@ -28,16 +32,20 @@ export const figuresContainer = {
     createPropPane(figure);
     this.refreshOrder();
   },
-  addGroup(groupName: string) {
+  addGroup(groupName: string | Group) {
     if (figuresContainer.groups.find(group => group.properties['groupName'].value === groupName)) {
       alert('Group "' + groupName + '" already exists');
-    } else {
+    } else if (groupName instanceof Group) {
+      this.groups.push(groupName);
+      this.refreshOrder();
+    }
+    else {
       const group = GroupFactory.createGroup(groupName);
       this.groups.push(group);
       this.refreshOrder();
     }
   },
-  clearElement(element: HTMLElement){
+  clearElement(element: HTMLElement) {
     while (element.firstChild) {
       element.firstChild.remove();
     }
@@ -46,17 +54,23 @@ export const figuresContainer = {
     const groupElement = document.createElement("li");
     const nameWrapper = this.createElement("span", `Group: ${group.properties["groupName"].value}`);
     const deleteButton = this.createButton("X", "delete-button", () => this.deleteGroup(group, groupElement));
+    const copyButton = this.createButton("Copy", "copy-button", () => this.copyGroup(group));
     const ul = document.createElement("ul");
 
     group.figures.forEach(fig => ul.appendChild(this.createFigureInGroupTreeElement(fig, group)));
 
     groupElement.appendChild(nameWrapper);
     groupElement.appendChild(deleteButton);
+    groupElement.appendChild(copyButton);
     groupElement.appendChild(ul);
 
     groupElement.addEventListener("click", (e) => {
-      if (e.target !== deleteButton) {
+      const clickedFigure = group.figures.find(fig => fig.properties['name'].value === (e.target as HTMLElement).textContent);
+
+      if (e.target !== deleteButton && (e.target as HTMLElement) === nameWrapper) {
         createGroupPropPane(group);
+      } else if (clickedFigure) {
+        createPropPane(clickedFigure)
       } else {
         closePropPane();
       }
@@ -121,6 +135,37 @@ export const figuresContainer = {
     button.classList.add(className);
     button.addEventListener("click", onClick);
     return button;
+  },
+
+  copyGroup(group: Group) {
+    const copiedGroup = GroupFactory.createGroup(group.properties['groupName'].value + "-copy");
+    group.figures.forEach(figure => {
+      const copiedFigure = Copy(figure);
+      if (copiedFigure) {
+        dragAndDropBootstrap(copiedFigure!);
+        copiedGroup.figures.push(copiedFigure);
+      }
+    });
+
+    copiedGroup!.properties = {};
+    Object.keys(group.properties).map((key) => {
+      const property = {
+        ...group!.properties[key],
+      };
+      const withProto = Object.setPrototypeOf(
+        property,
+        Object.getPrototypeOf(group!.properties[key])
+      ) as Property;
+      copiedGroup!.properties[key] = withProto;
+    });
+    copiedGroup!.properties["groupName"] = new NameProperty(
+      "groupName",
+      `${copiedGroup.properties["groupName"].value}-copy`
+    );
+
+    copiedGroup.refreshProperties();
+    this.refreshOrder();
+    this.addGroup(copiedGroup);
   },
 
   deleteFigure(fig: Figure, figureElement: HTMLElement) {
