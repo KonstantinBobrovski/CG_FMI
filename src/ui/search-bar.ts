8e308eebcbe8f510fe273c419d45aa9551e9284c
Velin Kurtev <velin.kurtev@dreambox.com>
2024-06-15 15:45:11 +0300

    Added changes

--- src/ui/search-bar.ts
+++ src/ui/search-bar.ts
@@ -1,32 +1,38 @@
 import { figuresContainer } from "../figures-container";
-import { createPropPane, closePropPane } from "./create-prop-pane";
+import Figure from "../models/figure";
+import Group from "../models/group";
+import { createPropPane, closePropPane, createGroupPropPane } from "./create-prop-pane";
 
 const searchInput: HTMLInputElement = document.querySelector("#search-input")!;
 
 export const initializeSearchInput = () => {
   searchInput.addEventListener("input", (e) => {
     const searchTerm = (e.target as HTMLInputElement).value.trim();
-    let figure = figuresContainer.figures.find(
-      (f) => f.properties.name.value.trim() === searchTerm
+    let figure: Figure | Group | undefined = figuresContainer.figures.find(
+      (f) => f.properties['name'].value.trim() === searchTerm
     );
-    
+
     if (!figure) {
       figuresContainer.groups.some((group) => {
         figure = group.figures.find(
-          (f) => f.properties.name.value.trim() === searchTerm
+          (f) => f.properties['name'].value.trim() === searchTerm
         );
-        return figure; 
+        return figure;
       });
     }
-    
+
     if (!figure) {
       figure = figuresContainer.groups.find(
-        (g) => g.properties.name.value.trim() === searchTerm
+        (g) => g.properties['groupName'].value.trim() === searchTerm
       );
     }
 
     if (figure) {
-      createPropPane(figure);
+      if (figure instanceof Group) {
+        createGroupPropPane(figure);
+      } else {
+        createPropPane(figure);
+      }
     } else {
       closePropPane();
     }
