bf7f42b32dc78f4f9f5047c10056a2bfee325fc4
Velin Kurtev <velin.kurtev@dreambox.com>
2024-06-15 15:32:01 +0300

    added search on groups and group items

--- src/ui/search-bar.ts
+++ src/ui/search-bar.ts
@@ -6,9 +6,25 @@ const searchInput: HTMLInputElement = document.querySelector("#search-input")!;
 export const initializeSearchInput = () => {
   searchInput.addEventListener("input", (e) => {
     const searchTerm = (e.target as HTMLInputElement).value.trim();
-    const figure = figuresContainer.figures.find(
+    let figure = figuresContainer.figures.find(
       (f) => f.properties.name.value.trim() === searchTerm
     );
+    
+    if (!figure) {
+      figuresContainer.groups.some((group) => {
+        figure = group.figures.find(
+          (f) => f.properties.name.value.trim() === searchTerm
+        );
+        return figure; 
+      });
+    }
+    
+    if (!figure) {
+      figure = figuresContainer.groups.find(
+        (g) => g.properties.name.value.trim() === searchTerm
+      );
+    }
+
     if (figure) {
       createPropPane(figure);
     } else {
